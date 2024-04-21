740f846170364956cbbcbc2c473c43056e7ff742
Manyu Lakhotia <mlakhotia@optimaldynamics.com>
2024-04-21 16:10:37 -0500

    +my own assets

--- lvls/battle_scene.gd
+++ lvls/battle_scene.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 @onready var battle_char_left = $battle_char_left;
+@onready var battle_char_right = $battle_char_right;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  var texture = preload("res://art/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png");
+  var texture = preload("res://art/my/chars/berserk_axer.png");
   if texture and texture is Texture:
-    battle_char_left.sprite.texture = texture;
-    battle_char_left.sprite.vframes = 4;
+    battle_char_right.sprite.texture = texture;
+  # battle_char_right.sprite.vframes = 4;
+  battle_char_left.sprite.flip_h = true;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
