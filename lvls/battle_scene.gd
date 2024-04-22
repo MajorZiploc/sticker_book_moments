2255abcd9ee162f9a3860797e779ed8a022a78b3
Manyu Lakhotia <mlakhotia@optimaldynamics.com>
2024-04-22 14:04:08 -0500

    +full stub sprite sheet

--- lvls/battle_scene.gd
+++ lvls/battle_scene.gd
@@ -5,10 +5,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  var texture = preload("res://art/my/chars/berserk_axer.png");
-  if texture and texture is Texture:
-    battle_char_right.sprite.texture = texture;
-  # battle_char_right.sprite.vframes = 4;
+  # var texture = preload("res://art/my/chars/berserk_axer.png");
+  # if texture and texture is Texture:
+    # battle_char_right.sprite.texture = texture;
   battle_char_left.sprite.flip_h = true;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
