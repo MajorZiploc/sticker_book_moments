0f3c9d6419bbc4c82bffe0793c06c59c5138d776
Manyu Lakhotia <lakemonyew@gmail.com>
2024-04-25 15:57:06 -0500

    +timer and basic ai

--- lvls/battle_scene.gd
+++ lvls/battle_scene.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var battle_char_left = $battle_char_left;
 @onready var battle_char_right = $battle_char_right;
+@onready var pause_after_player_atk = $PauseAfterPlayerAtk;
 
 @export var is_player_turn = true;
 
@@ -12,15 +13,14 @@ func _ready():
     # battle_char_right.sprite.texture = texture;
   battle_char_left.sprite.flip_h = true;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-  #if Input.is_action_just_pressed("ui_accept"):
-    #battle_char_right.take_damage(1);
-
-
 func _on_attack_pressed():
   if is_player_turn:
     battle_char_right.take_damage(1);
-  else:
+    is_player_turn = !is_player_turn;
+    pause_after_player_atk.start();
+
+func _on_pause_after_player_atk_timeout():
+  if !is_player_turn:
+    # TODO: abstrat this out into an AI decision system
     battle_char_left.take_damage(1);
-  is_player_turn = !is_player_turn;
+    is_player_turn = !is_player_turn;
