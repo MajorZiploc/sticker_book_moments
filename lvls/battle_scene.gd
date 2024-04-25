385f25ff6e2523fbe15d6cf77ed431da86cd578d
Manyu Lakhotia <lakemonyew@gmail.com>
2024-04-25 15:48:04 -0500

    +is_player_turn

--- lvls/battle_scene.gd
+++ lvls/battle_scene.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var battle_char_left = $battle_char_left;
 @onready var battle_char_right = $battle_char_right;
 
+@export var is_player_turn = true;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
   # var texture = preload("res://art/my/chars/berserk_axer.png");
@@ -17,4 +19,8 @@ func _ready():
 
 
 func _on_attack_pressed():
-  battle_char_right.take_damage(1);
+  if is_player_turn:
+    battle_char_right.take_damage(1);
+  else:
+    battle_char_left.take_damage(1);
+  is_player_turn = !is_player_turn;
