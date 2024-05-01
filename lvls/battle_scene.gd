a1385e326536a75805495226d6f3b0802d98b98d
Manyu Lakhotia <lakemonyew@gmail.com>
2024-05-01 13:08:48 -0500

    init parry marker

--- lvls/battle_scene.gd
+++ lvls/battle_scene.gd
@@ -11,13 +11,15 @@ class CombatUnit:
 
 @onready var player = CombatUnit.new($path_left/path_follow/battle_char, $path_left/path_follow, $path_left);
 @onready var npc = CombatUnit.new($path_right/path_follow/battle_char, $path_right/path_follow, $path_right);
+@onready var path_parry_marker: Path2D = $path_parry_marker;
+@onready var path_parry_marker_path_follow: PathFollow2D = $path_parry_marker/path_follow;
 
 @export var is_player_turn = true;
 
 var parry_attempted = false;
 var parry_attempted_ratio = 0.0;
-var min_parry_ratio = 0.5;
-var max_parry_ratio = 1.0;
+var min_parry_ratio = 0.65;
+var max_parry_ratio = 1;
 var player_init_position = Vector2(2, 0);
 var npc_init_position = Vector2(1036, 5);
 var attack_position_offset = Vector2(175, 0);
@@ -47,13 +49,18 @@ func _ready():
   npc.path.curve.clear_points();
   for point in basic_npc_path_points:
     npc.path.curve.add_point(point);
+    path_parry_marker.curve.add_point(point);
+  path_parry_marker_path_follow.progress_ratio = (min_parry_ratio + max_parry_ratio) / 2;
+  path_parry_marker.visible = false;
 
 func full_round(attacker: CombatUnit, defender: CombatUnit):
   await attack_sequence(attacker, defender);
   is_player_turn = !is_player_turn;
   await attack_sequence(defender, attacker);
 
+  # NOTE: in this func: is_player_turn actually means its enemy turn
 func attack_sequence(attacker: CombatUnit, defender: CombatUnit):
+  path_parry_marker.visible = is_player_turn;
   attacker.char.preatk();
   defender.char.readied();
   var tween = create_tween();
@@ -61,12 +68,12 @@ func attack_sequence(attacker: CombatUnit, defender: CombatUnit):
   await tween.finished;
   attacker.char.postatk();
   var damage_taker = defender;
-  # NOTE: this is actually the enemy turn if this is true at this point
   if is_player_turn:
     if parry_attempted and parry_attempted_ratio >= min_parry_ratio and parry_attempted_ratio <= max_parry_ratio:
       damage_taker = attacker;
       defender.char.postatk();
   damage_taker.char.take_damage(1);
+  path_parry_marker.visible = false;
   # HACK: to let the postatk frame show for a second
   tween = create_tween();
   tween.tween_property(attacker.path_follow, "progress_ratio", 1, 1);
