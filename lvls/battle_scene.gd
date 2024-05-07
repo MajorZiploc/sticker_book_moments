38885bc2e48a610eb9a12dfbe874820b9730dd67
Manyu Lakhotia <lakemonyew@gmail.com>
2024-05-07 11:08:02 -0500

    init qte in current battle loop

--- lvls/battle_scene.gd
+++ lvls/battle_scene.gd
@@ -49,20 +49,23 @@ class Background:
   $ui_root/ui/npc_info/hbox/bust,
 );
 @onready var path_parry_marker: Path2D = $path_parry_marker;
-@onready var path_parry_marker_path_follow: PathFollow2D = $path_parry_marker/path_follow;
+# @onready var path_parry_marker_path_follow: PathFollow2D = $path_parry_marker/path_follow;
 @onready var qte_container = $ui_root/ui/qte;
 @onready var qte_click_me_btn = $ui_root/ui/qte/click_me_btn;
 
 @export var is_player_turn = true;
 
-var parry_attempted = false;
-var parry_attempted_ratio = 0.0;
-var min_parry_ratio = 0.65;
-var max_parry_ratio = 1;
+var parried = false;
+# var parry_attempted = false;
+# var parry_attempted_ratio = 0.0;
+# var min_parry_ratio = 0.65;
+# var max_parry_ratio = 1;
 var player_init_position = Vector2(2, 0);
 var npc_init_position = Vector2(1020, 0);
 var attack_position_offset = Vector2(175, 0);
 var rng = RandomNumberGenerator.new();
+var qte_current_click_count = 0;
+var qte_click_total = 1;
 var qte_min_x = 100;
 var qte_max_x = 850;
 var qte_min_y = 160;
@@ -70,7 +73,7 @@ var qte_max_y = 540;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  qte_click_me_btn.visible = false;
+  qte_container.visible = false;
   _init_bg();
   player.unit_data = CombatUnitData.entries[CombatUnitData.Type.DUAL_HYBRID];
   npc.unit_data = CombatUnitData.entries[CombatUnitData.Type.TWO_HANDED_AXER];
@@ -91,14 +94,13 @@ func _ready():
   npc.path.curve.clear_points();
   for point in npc_path_points:
     npc.path.curve.add_point(point);
-    path_parry_marker.curve.add_point(point);
-  path_parry_marker_path_follow.progress_ratio = (min_parry_ratio + max_parry_ratio) / 2;
+    # path_parry_marker.curve.add_point(point);
+  # path_parry_marker_path_follow.progress_ratio = (min_parry_ratio + max_parry_ratio) / 2;
   path_parry_marker.visible = false;
   player.name.text = player.unit_data.name;
   npc.name.text = npc.unit_data.name;
   _update_unit_health_bar(player);
   _update_unit_health_bar(npc);
-  qte_click_me_btn.visible = true;
 
 func update_bust_texture(combat_unit: CombatUnit):
   var texture = load(combat_unit.unit_data.bust_path);
@@ -119,7 +121,8 @@ func deal_damage_to(combat_unit: CombatUnit):
 
 # NOTE: in this func: is_player_turn actually means its enemy turn
 func attack_sequence(attacker: CombatUnit, defender: CombatUnit):
-  path_parry_marker.visible = is_player_turn;
+  # path_parry_marker.visible = is_player_turn;
+  qte_container.visible = is_player_turn;
   attacker.char.preatk();
   defender.char.readied();
   var tween = create_tween();
@@ -127,12 +130,13 @@ func attack_sequence(attacker: CombatUnit, defender: CombatUnit):
   await tween.finished;
   attacker.char.postatk();
   var damage_taker = defender;
-  if is_player_turn:
-    if parry_attempted and parry_attempted_ratio >= min_parry_ratio and parry_attempted_ratio <= max_parry_ratio:
-      damage_taker = attacker;
-      defender.char.postatk();
+  if is_player_turn and parried:
+    # if parry_attempted and parry_attempted_ratio >= min_parry_ratio and parry_attempted_ratio <= max_parry_ratio:
+    damage_taker = attacker;
+    defender.char.postatk();
   deal_damage_to(damage_taker);
-  path_parry_marker.visible = false;
+  # path_parry_marker.visible = false;
+  qte_container.visible = false;
   # HACK: to let the postatk frame show for a second
   await get_tree().create_timer(1).timeout;
   attacker.char.idle();
@@ -144,12 +148,13 @@ func attack_sequence(attacker: CombatUnit, defender: CombatUnit):
 func _on_attack_pressed():
   if is_player_turn and player.path_follow.progress_ratio == 0 and npc.path_follow.progress_ratio == 0:
     is_player_turn = !is_player_turn;
-    parry_attempted = false;
-    parry_attempted_ratio = 0.0;
+    parried = false;
+    qte_current_click_count = 0;
+    # parry_attempted_ratio = 0.0;
     full_round(player, npc);
-  elif !parry_attempted:
-    parry_attempted = true;
-    parry_attempted_ratio = npc.path_follow.progress_ratio;
+  # elif !parry_attempted:
+  #   parry_attempted = true;
+  #   parry_attempted_ratio = npc.path_follow.progress_ratio;
 
 func _init_bg_cloud_movements(clouds: Array[Sprite2D], start_x: float, end_x: float, total_move_secs: float, spacer: float):
   for cloud in clouds:
@@ -175,9 +180,11 @@ func _init_bg():
   _init_bg_cloud_movements(background.sm_clouds, -800, 2500, 90, 0.1);
 
 func _on_qte_click_me_btn_pressed():
-  qte_container.position = Vector2(
-    rng.randf_range(qte_min_x, qte_max_x),
-    rng.randf_range(qte_min_y, qte_max_y)
-  );
-  deal_damage_to(npc);
-
+  if qte_current_click_count < qte_click_total:
+    qte_current_click_count = qte_current_click_count + 1;
+    parried = qte_current_click_count == qte_click_total;
+    qte_container.position = Vector2(
+      rng.randf_range(qte_min_x, qte_max_x),
+      rng.randf_range(qte_min_y, qte_max_y)
+    );
+    qte_container.visible = !parried;
