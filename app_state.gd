4628a5fd1c9d2bc9c4e1e0bdfadb5016057d55b3
Manyu Lakhotia <lakemonyew@gmail.com>
2024-05-15 16:44:46 -0500

    ^delete to delete data in memory aswell;more data chaining;

--- app_state.gd
+++ app_state.gd
@@ -10,13 +10,17 @@ func load_data(data_file):
   if FileAccess.file_exists(data_file["name"]):
     var file = FileAccess.open(data_file["name"], FileAccess.READ)
     data = file.get_var();
+  return data;
 
 func save_data(new_app_state, data_file):
   var file = FileAccess.open(data_file["name"], FileAccess.WRITE)
   file.store_var(new_app_state);
+  return new_app_state;
 
 func delete_data(data_file):
-  DirAccess.remove_absolute(data_file["name"])
+  DirAccess.remove_absolute(data_file["name"]);
+  data = {};
+  return data;
 
 func load_session():
   return load_data(current_data_file_name);
