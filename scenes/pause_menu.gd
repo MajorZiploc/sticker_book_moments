4defb5b83294bd50ca8b769ab986cfafa7398c1a
Manyu Lakhotia <lakemonyew@gmail.com>
2024-05-17 19:39:03 -0500

    Add back button to pause menu

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_quit_button_up():
   get_tree().quit();
+
+func _on_back_button_up():
+  var scene_path = SceneSwitcher.get_param("return_scene");
+  SceneSwitcher.change_scene(scene_path, {});
--- utils/scene_helper.gd
+++ utils/scene_helper.gd
@@ -2,4 +2,4 @@ extends Node
 
 func process_input(event: InputEvent):
   if event.is_action_pressed("pause"):
-    SceneSwitcher.change_scene("res://scenes/pause_menu.tscn", {});
+    SceneSwitcher.change_scene("res://scenes/pause_menu.tscn", { "return_scene": get_tree().current_scene.scene_file_path });
