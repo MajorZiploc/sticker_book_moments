20afb219668d6651893d2153613411b5e5a6b93c
Manyu Lakhotia <lakemonyew@gmail.com>
2024-05-19 06:53:51 -0500

    rn full screen btn

--- scenes/options_menu.gd
+++ scenes/options_menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _on_back_button_up():
   SceneSwitcher.change_scene("res://scenes/pause_menu.tscn", SceneSwitcher.get_params());
 
-func _on_test_button_up():
+func _on_fullscreen_toggle_button_up():
   var window_mode = (
     DisplayServer.WINDOW_MODE_FULLSCREEN if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED
     else DisplayServer.WINDOW_MODE_WINDOWED
