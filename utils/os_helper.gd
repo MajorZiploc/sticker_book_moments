b71af049f8467ad844afac68b8b0f8937273ec31
Manyu Lakhotia <lakemonyew@gmail.com>
2024-05-19 06:15:04 -0500

    +is_web();+list of feature tags

--- utils/os_helper.gd
+++ utils/os_helper.gd
@@ -1,6 +1,53 @@
 extends Node
 
+# TODO: test these is_* flags
+
 func is_mobile():
-  # TODO: test this on mobile devices. the OS.has_feature should be enough. the rest likely isnt needed
-  var os_name = OS.get_name();
-  return OS.has_feature("mobile") or os_name == "Android" or os_name == "iOS";
+  # var os_name = OS.get_name();
+  return OS.has_feature("mobile");
+
+func is_web():
+  # var os_name = OS.get_name();
+  return OS.has_feature("web");
+
+# default feature_tags list: (from: https://docs.godotengine.org/en/stable/tutorials/export/feature_tags.html)
+# android
+# bsd
+# linux
+# macos
+# ios
+# windows
+# linuxbsd
+# debug
+# release
+# editor
+# template
+# double
+# single
+# 64
+# 32
+# x86_64
+# x86_32
+# x86
+# arm64
+# arm32
+# arm
+# rv64
+# riscv
+# ppc64
+# ppc32
+# ppc
+# wasm64
+# wasm32
+# wasm
+# mobile
+# pc
+# web
+# web_android
+# web_ios
+# web_linuxbsd
+# web_macos
+# web_windows
+# etc
+# etc2
+# s3tc
