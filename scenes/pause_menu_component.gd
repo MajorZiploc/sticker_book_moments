f755b5e89a584f21f26c64668d65713f23d54e4a
Manyu Lakhotia <lakemonyew@gmail.com>
2024-05-21 18:48:34 -0500

    wire pause component for fn

---
+++ scenes/pause_menu_component.gd
@@ -0,0 +1,28 @@
+extends Control
+class_name PauseMenuComponent
+
+@onready var qte_type_option_btn: OptionButton = $tabs/options/vbox/qte_type/selector;
+
+func _ready():
+  var qte_type_option_popup = qte_type_option_btn.get_popup();
+  qte_type_option_popup.connect("id_pressed", on_qte_type_option_popup_pressed);
+  qte_type_option_btn.selected = AppState.data.get("options", {}).get("qte_mode", -1);
+
+func _on_back_button_up():
+  get_parent().remove_child(self);
+
+func _on_fullscreen_toggle_button_up():
+  var window_mode = (
+    DisplayServer.WINDOW_MODE_FULLSCREEN if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED
+    else DisplayServer.WINDOW_MODE_WINDOWED
+  );
+  OptionsHelper.set_window_mode(window_mode);
+  AppState.insert_data("options", { "window_mode": window_mode });
+  AppState.save_session();
+
+func on_qte_type_option_popup_pressed(qte_mode):
+  AppState.insert_data("options", { "qte_mode": qte_mode });
+  AppState.save_session();
+
+func _on_quit_button_up():
+  get_tree().quit();
